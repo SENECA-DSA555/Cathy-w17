8d3b11085985cbae4408174ab52635fabbed873d
cathyatseneca <cathyatseneca@gmail.com>
2017-01-16 16:39:51 -0500

    power function done

--- lab1.cpp
+++ lab1.cpp
@@ -4,7 +4,11 @@ unsigned int factorial(unsigned int n){
 	
 }
 double power (double base, unsigned int n){
-	
+	double rc=1;
+	for(int i=n;i!=0;i--){
+		rc=rc*base;
+	}
+	return rc;
 }
 unsigned int fibonacci (unsigned int n){
 	
